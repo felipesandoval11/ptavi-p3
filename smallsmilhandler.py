1219f67efec4484898c27040b3b38cb1f447923d
Felipe Sandoval <felipesandoval@MacBookPro-de-Felipe.local>
2016-10-05 01:54:54 +0200

    modificando la estructura de la lista de etiquetas

--- smallsmilhandler.py
+++ smallsmilhandler.py
@@ -5,8 +5,6 @@
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
 
-mydata = {}
-mylistdata = []
 
 class SmallSMILHandler(ContentHandler):
     def __init__(self):
@@ -22,46 +20,44 @@ class SmallSMILHandler(ContentHandler):
         self.begin = ""
         self.dur = ""
         self.region = ""
+        self.mytags = []
 
     def startElement(self, name, attr):
         if name == "root-layout":
-            self.width = attr.get('width', "")
-            self.height = attr.get('height', "")
-            self.backgroundcolor = attr.get('background-color', "")
+            width = {"width" : attr.get('width', "")}
+            width["height"] = attr.get('height', "")
+            width["background-color"] = attr.get('background-color', "")
+            tagroot = {"root-layout": width}
+            self.mytags.append(tagroot)
         elif name == "region":
             self.id = attr.get('id', "")
             self.bottom = attr.get('bottom', "")
             self.left = attr.get('left', "")
             self.top = attr.get('top', "")
-            mydata.update({name:[self.id, self.bottom,
-            self.left, self.bottom]})
-            mylistdata.append(mydata)
+
         elif name == "img":
             self.region = attr.get('region', "")
             self.src = attr.get('src', "")
             self.begin = attr.get('begin', "")
             self.dur = attr.get('dur', "")
-            mydata.update({name:[self.src, self.region,
-            self.begin, self.dur]})
-            mylistdata.append(mydata)
+
         elif name == "audio":
             self.src = attr.get('src', "")
             self.begin = attr.get('begin', "")
             self.dur = attr.get('dur', "")
-            mydata.update({name:[self.src, self.begin,
-            self.dur]})
-            mylistdata.append(mydata)
+
         elif name == "textstream":
             self.src = attr.get('src', "")
             self.region = attr.get('region', "")
-            mydata.update({name:[self.src, self.region]})
-            mylistdata.append(mydata)
-
+    
     def get_tags(self):
-        return self.mydata
+        return self.mytags
 
 if __name__ == "__main__":
     parser = make_parser()
     sHandler = SmallSMILHandler()
     parser.setContentHandler(sHandler)
     parser.parse(open('karaoke.smil'))
+    mytags = sHandler.get_tags()
+    for data in mytags:
+        print(data)
