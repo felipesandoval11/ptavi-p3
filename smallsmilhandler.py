28256d5ee9e1842bd481151ffa4faaab7e33a766
Felipe Sandoval <felipesandoval@MacBookPro-de-Felipe.local>
2016-10-08 01:45:02 +0200

    he quitado instancias que no hacian nada

--- smallsmilhandler.py
+++ smallsmilhandler.py
@@ -8,18 +8,6 @@ from xml.sax.handler import ContentHandler
 
 class SmallSMILHandler(ContentHandler):
     def __init__(self):
-        self.width = ""
-        self.height = ""
-        self.backgroundcolor = ""
-        self.id = ""
-        self.top = ""
-        self.bottom = ""
-        self.left = ""
-        self.right = ""
-        self.src = ""
-        self.begin = ""
-        self.dur = ""
-        self.region = ""
         self.mytags = []
 
     def startElement(self, name, attr):
