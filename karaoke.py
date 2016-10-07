1802ee6c126c14947ed4c3256efe4c58285a5700
Felipe Sandoval <felipesandoval@MacBookPro-de-Felipe.local>
2016-10-07 23:04:19 +0200

    terminado apartado 5 con pep8

--- karaoke.py
+++ karaoke.py
@@ -2,14 +2,17 @@
 # -*- coding: utf-8 -*-
 # Made by Felipe Sandoval Sibada
 
-from xml.sax import make_parser
 import sys
+import json
+from xml.sax import make_parser
 from smallsmilhandler import SmallSMILHandler
+from urllib.request import urlretrieve
 
 
 if __name__ == "__main__":
     try:
-        attrval = " "
+        attrval = ""
+        datatags = ""
         parser = make_parser()
         sHandler = SmallSMILHandler()
         parser.setContentHandler(sHandler)
@@ -18,9 +21,17 @@ if __name__ == "__main__":
         for data in mytags:
             for elem, attr in data.items():
                 for k, v in attr.items():
+                    if k == "src" and v != "" and len(v) > 7:  # just in case
+                        if v[0:7] == "http://":
+                            urlretrieve(v, v.split("/")[-1])
+                            v = v.split("/")[-1]
                     if v != "":         # omitting null values
                         attrval += "\t" + k + "=" + '"' + v + '"'
                 print(elem + attrval)
+                datatags += elem + attrval
                 attrval = " "
+        filename = sys.argv[1].split(".")[0] + ".json"  # file .json = .smil
+        with open(filename, "w") as outfile:
+            json.dump(datatags,  outfile)
     except IndexError:
         print("Usage: python3 karaoke.py file.smil.")
