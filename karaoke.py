f9714174126e5f2b677007e06b76ed1d166c67a8
Felipe Sandoval <felipesandoval@MacBookPro-de-Felipe.local>
2016-10-07 20:11:56 +0200

    funciona bien, pep8 y optimizado

--- karaoke.py
+++ karaoke.py
@@ -6,8 +6,10 @@ from xml.sax import make_parser
 import sys
 from smallsmilhandler import SmallSMILHandler
 
+
 if __name__ == "__main__":
     try:
+        attrval = " "
         parser = make_parser()
         sHandler = SmallSMILHandler()
         parser.setContentHandler(sHandler)
@@ -15,12 +17,10 @@ if __name__ == "__main__":
         mytags = sHandler.get_tags()
         for data in mytags:
             for elem, attr in data.items():
-                if int(len(data)) == 0:
-                    print(elem)
-                else:
-                    print('\n' + elem, end='')
                 for k, v in attr.items():
-                    attrval = k + '"' + v + '"'
-                    print("\t"+ k + "=" + '"' + v + '"', end=' ')
+                    if v != "":         # omitting null values
+                        attrval += "\t" + k + "=" + '"' + v + '"'
+                print(elem + attrval)
+                attrval = " "
     except IndexError:
         print("Usage: python3 karaoke.py file.smil.")
